778303fcdbfadfdf311126c5f95e96d78319d663
Brendan-McC <56608332+Brendan-McC@users.noreply.github.com>
2020-05-09 11:39:10 -0400

    update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+self.addEventListener('install', async event => {
+  console.log('install event')
+});
+
+self.addEventListener('fetch', async event => {
+  console.log('fetch event')
+});
+
 const cacheName = 'pwa-conf-v1';
 const staticAssets = [
   './',
@@ -5,3 +13,8 @@ const staticAssets = [
   './app.js',
   './basic.css'
 ];
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(cacheName); 
+  await cache.addAll(staticAssets); 
+});
