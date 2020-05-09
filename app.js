e0c4e807eaf582e6d6c53d3fd946b7f2dc8f26eb
Brendan-McC <56608332+Brendan-McC@users.noreply.github.com>
2020-05-09 11:00:21 -0400

    update

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
+class RandysApp{}
+
 window.addEventListener('load', e => {
-    new RandysPWA();
-    registerSW(); 
+    new RandysApp();
+    registerSW();
   });
 
-async function registerSW() { 
+  async function registerSW() { 
     if ('serviceWorker' in navigator) { 
-    try {
+      try {
         await navigator.serviceWorker.register('./sw.js'); 
-    } catch (e) {
+      } catch (e) {
         alert('ServiceWorker registration failed. Sorry about that.'); 
-    }
+      }
     } else {
-    document.querySelector('.alert').removeAttribute('hidden'); 
+      document.querySelector('.alert').removeAttribute('hidden'); 
     }
-}
-
-self.addEventListener('install', async event => {
-    console.log('install event')
-});
-
-self.addEventListener('fetch', async event => {
-    console.log('fetch event')
-});
+  }
